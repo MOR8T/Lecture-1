1347f2ea70b303833a45997a16f895a26bb46b8a
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-11-02 16:46:12 +0500

    commit3

--- src/App.js
+++ src/App.js
@@ -40,20 +40,39 @@ function App() {
         <h2>root.render()</h2>
         <div className='js7'></div>
         <h1>Компоненты и пропсы</h1>
+        <h2>#1</h2>
         <div className='js8'></div>
+        <h2>#2</h2>
         <div className='js9'></div>
         <div className='js10'></div>
         <p>{elem2}</p>
+        <h2>#3</h2>
         <div className='js11'></div>
         <APP />
+        <h2>#4</h2>
+        <div className='js12'></div>
+        <h2>#4.1</h2>
+        <div className='js13'></div>
+        <h2>#4.2</h2>
+        <div className='js14'></div>
+        <h2>#4.3</h2>
+        <div className='js15'></div>
+        <h2>#4.4</h2>
+        <div className='js16'></div>
+        <h2>#5  чистый function</h2>
+        <div className='js17'></div>
+        <h2>#6 нечистой function</h2>
+        <div className='js18'></div>
+        <h2>React-компоненты обязаны вести себя как чистые функции по отношению к своим пропсам.</h2>
       </div>
-      <div className='container-3'></div>
-      <div className='container-4'></div>
-      <div className='container-5'></div>
     </div>
   );
 }
 
 export default App;
 
+
+function withdraw(account, amount) {
+  account.total -= amount;
+}
 
