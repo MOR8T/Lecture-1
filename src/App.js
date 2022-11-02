d20b340057b553373a07d54d91b8bcfd532ff3d6
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-11-02 15:03:33 +0500

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-max'>
+      <div className='container-1'></div>
+      <div className='container-2'>
+        <h1>JSX</h1>
+        <h2>#1</h2>
+        <div className='js1'></div>
+        <h2>#2 function / object</h2>
+        <div className='js2'></div>
+        <h2>#3 function</h2>
+        <div className='js3'></div>
+        <h2>#4 atribu</h2>
+        <div className='js4'></div>
+        <h2>#5 React.createElement()</h2>
+        <div className='js5'></div>
+        <h2>#6</h2>
+        <div className='js6'></div>
+        <h1>Рендеринг элементов</h1>
+      </div>
+      <div className='container-3'></div>
+      <div className='container-4'></div>
+      <div className='container-5'></div>
     </div>
   );
 }
 
 export default App;
+
+
+const element = {
+  type: 'h1',
+  props: {
+    className: 'H1',
+    children: 'Hello'
+  }
+};
+
